27a1797f7b556be98d90916e5937bdc0af400f10
VPresich <111353667+VPresich@users.noreply.github.com>
2024-01-20 12:45:25 +0000

    Deploying to gh-pages from @ VPresich/classwork-timer@cb377eff8c9d8e8dabe35f06424cb95d59796017 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,93 +1,21 @@
-import"./assets/styles-0c304372.js";import{T as g}from"./assets/constants-60c1b47f.js";const d=[{name:"Mark Guerrero",photo:{fileName:"card-1@1x.jpg",alt:"Photo of Mark Guerrero"},position:"Product Designer"},{name:"Tom Ford",photo:{fileName:"card-2@1x.jpg",alt:"Photo of Tom Ford"},position:"Frontend Developer"},{name:"Camila Garcia",photo:{fileName:"card-3@1x.jpg",alt:"Photo of Camila Garcia"},position:"Marketing"},{name:"Daniel Wilson",photo:{fileName:"card-4@1x.jpg",alt:"Photo of Daniel Wilson"},position:"UI Designer"}],x="/classwork-timer/assets/icons-ad39d7a4.svg",n=(t,i)=>{const{iconContainerClass:o,iconLinkClass:e,iconClass:p,iconWidth:r,iconHeight:l}=i;return t.reduce((a,s)=>a+`
+import"./assets/styles-0c304372.js";import{T as d}from"./assets/constants-60c1b47f.js";const h=[{name:"Mark Guerrero",photo:{fileName:"card-1@1x.jpg",alt:"Photo of Mark Guerrero"},position:"Product Designer"},{name:"Tom Ford",photo:{fileName:"card-2@1x.jpg",alt:"Photo of Tom Ford"},position:"Frontend Developer"},{name:"Camila Garcia",photo:{fileName:"card-3@1x.jpg",alt:"Photo of Camila Garcia"},position:"Marketing"},{name:"Daniel Wilson",photo:{fileName:"card-4@1x.jpg",alt:"Photo of Daniel Wilson"},position:"UI Designer"}],p="/classwork-timer/assets/icons-ad39d7a4.svg",n=(a,i)=>{const{iconContainerClass:o,iconLinkClass:e,iconClass:t,iconWidth:l,iconHeight:r}=i;return a.reduce((m,s)=>m+`
         <li class="${o}">
                 <a class="${e}" target="_blank" href="${s.url}">
-                    <svg class="${p}" 
-                    width="${r}" height="${l}" aria-label="${s.label}">
-                    <use href="${x}${s.id}"></use>
+                    <svg class="${t}" 
+                    width="${l}" height="${r}" aria-label="${s.label}">
+                    <use href="${p}${s.id}"></use>
                     </svg>
                 </a>
         </li>
-    `,"")},m=[{url:"https://www.instagram.com/",id:"#instagram",label:"instagram icon"},{url:"https://www.twitter.com/",id:"#twitter",label:"twitter icon"},{url:"https://www.facebook.com/",id:"#facebook",label:"facebook icon"},{url:"https://www.linkedin.com/",id:"#linkedin",label:"linkedin icon"}],j={iconContainerClass:"social-icon-container",iconLinkClass:"social-link",iconClass:"social-icon",iconWidth:16,iconHeight:16},f=t=>t.reduce((i,{name:o,photo:e,position:p})=>i+`
+    `,"")},c=[{url:"https://www.instagram.com/",id:"#instagram",label:"instagram icon"},{url:"https://www.twitter.com/",id:"#twitter",label:"twitter icon"},{url:"https://www.facebook.com/",id:"#facebook",label:"facebook icon"},{url:"https://www.linkedin.com/",id:"#linkedin",label:"linkedin icon"}];function f(a,i){const o=a+i;console.log(o);const e=new URL(o,import.meta.url).href;return console.log(e),e}const u={iconContainerClass:"social-icon-container",iconLinkClass:"social-link",iconClass:"social-icon",iconWidth:16,iconHeight:16},g=a=>a.reduce((i,{name:o,photo:e,position:t})=>i+`
         <li class="team-card">
-                  <img src="${g}${e.fileName}" 
-                      alt="${e.alt}" width="264" height="260">
-                  <div class="team-card-label">
-                      <h3 class="team-card-name">${o}</h3>
-                      <p class="team-card-position">${p}</p>
-                      <ul class = "social-links">${n(m,j)}</ul>
-                  </div>
+          <img src="${f(d,e.fileName)}" 
+               alt="${e.alt}" width="264" height="260">
+          <div class="team-card-label">
+              <h3 class="team-card-name">${o}</h3>
+              <p class="team-card-position">${t}</p>
+              <ul class = "social-links">${n(c,u)}</ul>
+          </div>
         </li>
-      `,""),b=(t,i)=>{const o=f(t);i.innerHTML="",i.insertAdjacentHTML("beforeend",o)},w=document.querySelector(".team");b(d,w);const c=[{name:"Banking App",type:"App",imgSrc:"./img/portfolio/mobile/prj-mobile-1-1@1x.jpg",imgSrcset:`
-        ./img/portfolio/desktop/prj-desktop-1-1@1x.jpg 360w,
-        ./img/portfolio/desktop/prj-desktop-1-1@2x.jpg 720w,
-        ./img/portfolio/tablet/prj-tablet-1-1@1x.jpg   356w,
-        ./img/portfolio/tablet/prj-tablet-1-1@2x.jpg   712w,
-        ./img/portfolio/mobile/prj-mobile-1-1@1x.jpg   288w,
-        ./img/portfolio/mobile/prj-mobile-1-1@2x.jpg   576w
-      `,sizes:"(min-width:1158px) 360px, (min-width:768px) 356px, (max-width:767px) 288px",overlayText:`14 Stylish and User-Friendly App Design Concepts · Task Manager App
-     · Calorie Tracker App · Exotic Fruit Ecommerce App · Cloud Storage App`},{name:"Cashless Payment",type:"Marketing",imgSrc:"./img/portfolio/mobile/prj-mobile-1-2@1x.jpg",imgSrcset:`       
-        ./img/portfolio/desktop/prj-desktop-1-2@1x.jpg 360w,
-        ./img/portfolio/desktop/prj-desktop-1-2@2x.jpg 720w,
-        ./img/portfolio/tablet/prj-tablet-1-2@1x.jpg   356w,
-        ./img/portfolio/tablet/prj-tablet-1-2@2x.jpg   712w,
-        ./img/portfolio/mobile/prj-mobile-1-2@1x.jpg   288w,
-        ./img/portfolio/mobile/prj-mobile-1-2@2x.jpg   576w
-        `,sizes:"(min-width:1158px) 360px, (min-width:768px) 356px, (max-width:767px) 288px",overlayText:`14 Stylish and User-Friendly App Design Concepts · Task
-                    Manager App · Calorie Tracker App · Exotic Fruit Ecommerce
-                    App · Cloud Storage App`},{name:"Meditation App",type:"App",imgSrc:"./img/portfolio/mobile/prj-mobile-1-3@1x.jpg",imgSrcset:`       
-        ./img/portfolio/desktop/prj-desktop-1-3@1x.jpg 360w,
-        ./img/portfolio/desktop/prj-desktop-1-3@2x.jpg 720w,
-        ./img/portfolio/tablet/prj-tablet-1-3@1x.jpg   356w,
-        ./img/portfolio/tablet/prj-tablet-1-3@2x.jpg   712w,
-        ./img/portfolio/mobile/prj-mobile-1-3@1x.jpg   288w,
-        ./img/portfolio/mobile/prj-mobile-1-3@2x.jpg   576w
-        `,sizes:"(min-width:1158px) 360px, (min-width:768px) 356px, (max-width:767px) 288px",overlayText:`14 Stylish and User-Friendly App Design Concepts · Task
-                    Manager App · Calorie Tracker App · Exotic Fruit Ecommerce
-                    App · Cloud Storage App`},{name:"Taxi Service",type:"Marketing",imgSrc:"./img/portfolio/mobile/prj-mobile-2-1@1x.jpg",imgSrcset:`       
-      ./img/portfolio/desktop/prj-desktop-2-1@1x.jpg 360w,
-      ./img/portfolio/desktop/prj-desktop-2-1@2x.jpg 720w,
-      ./img/portfolio/tablet/prj-tablet-2-1@1x.jpg   356w,
-      ./img/portfolio/tablet/prj-tablet-2-1@2x.jpg   712w,
-      ./img/portfolio/mobile/prj-mobile-2-1@1x.jpg   288w,
-      ./img/portfolio/mobile/prj-mobile-2-1@2x.jpg   576w
-        `,sizes:"(min-width:1158px) 360px, (min-width:768px) 356px, (max-width:767px) 288px",overlayText:`14 Stylish and User-Friendly App Design Concepts · Task
-          Manager App · Calorie Tracker App · Exotic Fruit Ecommerce
-          App · Cloud Storage App`},{name:"Screen Illustrations",type:"Design",imgSrc:"./img/portfolio/mobile/prj-mobile-2-2@1x.jpg",imgSrcset:`       
-    ./img/portfolio/desktop/prj-desktop-2-2@1x.jpg 360w,
-    ./img/portfolio/desktop/prj-desktop-2-2@2x.jpg 720w,
-    ./img/portfolio/tablet/prj-tablet-2-2@1x.jpg   356w,
-    ./img/portfolio/tablet/prj-tablet-2-2@2x.jpg   712w,
-    ./img/portfolio/mobile/prj-mobile-2-2@1x.jpg   288w,
-    ./img/portfolio/mobile/prj-mobile-2-2@2x.jpg   576w
-        `,sizes:"(min-width:1158px) 360px, (min-width:768px) 356px, (max-width:767px) 288px",overlayText:`14 Stylish and User-Friendly App Design Concepts · Task
-        Manager App · Calorie Tracker App · Exotic Fruit Ecommerce
-        App · Cloud Storage App`},{name:"Online Courses",type:"Marketing",imgSrc:"./img/portfolio/mobile/prj-mobile-2-3@1x.jpg",imgSrcset:`       
-      ./img/portfolio/desktop/prj-desktop-2-3@1x.jpg 360w,
-      ./img/portfolio/desktop/prj-desktop-2-3@2x.jpg 720w,
-      ./img/portfolio/tablet/prj-tablet-2-3@1x.jpg   356w,
-      ./img/portfolio/tablet/prj-tablet-2-3@2x.jpg   712w,
-      ./img/portfolio/mobile/prj-mobile-2-3@1x.jpg   288w,
-      ./img/portfolio/mobile/prj-mobile-2-3@2x.jpg   576w
-        `,sizes:"(min-width:1158px) 360px, (min-width:768px) 356px, (max-width:767px) 288px",overlayText:`14 Stylish and User-Friendly App Design Concepts · Task
-        Manager App · Calorie Tracker App · Exotic Fruit Ecommerce
-        App · Cloud Storage App`}],k=t=>t.reduce((i,{name:o,type:e,imgSrc:p,imgSrcset:r,sizes:l,overlayText:a})=>i+`
-        <li class="portfolio-card">
-        <a class="portfolio-card-link" href="#">
-          <div class="portfolio-img-container">
-            <img
-              class="portfolio-img"
-              srcset="${r}"
-              sizes="${l}"
-              src="${p}"
-              alt="Photo of ${o} project"
-            />
-            <p class="portfolio-overlay">${a}</p>
-          </div>
-          <div class="portfolio-item-label">
-            <h3 class="portfolio-item-name">${o}</h3>
-            <p class="portfolio-item-type">${e}</p>
-          </div>
-        </a>
-      </li>
-      `,""),h=(t,i)=>{const o=k(t);console.log(o),i.innerHTML="",i.insertAdjacentHTML("beforeend",o)};console.log(c);const u=document.querySelector(".portfolio-cards");h(c,u);const A=(t,i,o)=>{const e=n(t,i);o.innerHTML="",o.insertAdjacentHTML("beforeend",e)},C={iconContainerClass:"footer-social-icon-container",iconLinkClass:"footer-social-link",iconClass:"footer-social-icon",iconWidth:24,iconHeight:24},T=document.querySelector(".footer-social-links");A(m,C,T);
+      `,""),k=(a,i)=>{const o=g(a);i.innerHTML="",i.insertAdjacentHTML("beforeend",o)},w=document.querySelector(".team");k(h,w);const C=(a,i,o)=>{const e=n(a,i);o.innerHTML="",o.insertAdjacentHTML("beforeend",e)},$={iconContainerClass:"footer-social-icon-container",iconLinkClass:"footer-social-link",iconClass:"footer-social-icon",iconWidth:24,iconHeight:24},b=document.querySelector(".footer-social-links");C(c,$,b);
 //# sourceMappingURL=commonHelpers2.js.map

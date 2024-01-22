3a1fba13fa3490096ae432538f8f63621ee61f27
VPresich <111353667+VPresich@users.noreply.github.com>
2024-01-22 14:30:25 +0000

    Deploying to gh-pages from @ VPresich/classwork-timer@383a9684e602266f6e7afbfac72f94d0ee660d9b 🚀

--- commonHelpers8.js
+++ commonHelpers8.js
@@ -1,4 +1,4 @@
-import"./assets/styles-2330ef24.js";import{f as a}from"./assets/fetch-data-async-fad340f3.js";import{U as n}from"./assets/constants-0f6b14ca.js";import{c as s,a as c}from"./assets/create-msg-41c53447.js";import"./assets/vendor-32231325.js";function i(r){return r.reduce((t,{title:e,completed:o})=>t+`<li>
+import"./assets/styles-2330ef24.js";import{f as a}from"./assets/fetch-data-async-fad340f3.js";import{U as n}from"./assets/constants-0f6b14ca.js";import{c as s,a as c}from"./assets/create-msg-2c2204d9.js";import"./assets/vendor-60fbd1ed.js";function i(r){return r.reduce((t,{title:e,completed:o})=>t+`<li>
         <div>${e}</div>
         <div>${o?"Done":"In progress"}</div>  
       </li>
